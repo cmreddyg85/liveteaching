8362e320b0885b31f8706c72f9cf4efd6e5f8aa8
hasenp <106738495+hasenp@users.noreply.github.com>
2026-01-03 15:34:27 +0530

    Update java-data.js
    pushign the changes

--- src/java-data.js
+++ src/java-data.js
@@ -1,12 +1,17 @@
-// ================= SPRING BOOT =================
-
-const commands = [
+const commandss = [
   {
-    command: "What is Spring Boot?",
-    text: `<p>Spring Boot is a framework built on top of Spring that simplifies application development.</p>
-      <li>Provides auto-configuration.</li>
-      <li>Reduces boilerplate code.</li>
-      <li>Uses embedded servers like Tomcat.</li>
-    `,
+    command: "Introduction",
+    text: `<p>Hii! I am [Your Name],
+  
+          From the past five years, I've been dedicated to develop user-friendly and visually appealing web applications as a front-end developer. My primary focus has been on utilizing React.js to build interactive and dynamic user interfaces.</p>
+          
+          <p>I have a strong understanding of HTML, CSS, and JavaScript, which are the building blocks of web development. With React.js, I've been able to create reusable components, manage state efficiently, and ensure high performance in web applications.</p>
+          
+          <p>Throughout my career, I've worked on various projects where I've collaborated closely with designers and backend developers to turn concepts into reality. I'm experienced in translating design mockups into responsive and perfect layouts, ensuring a seamless user experience across different devices and screen sizes.</p>
+          
+          
+          <p> I'm always eager to stay updated with the latest trends and best practices in front-end development to deliver the best possible solutions.</p>
+          
+       `,
   },
-];
+  ]
